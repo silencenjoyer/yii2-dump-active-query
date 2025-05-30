f25f2ef859183e03636611895000ea4980a39214
Sikencenjoyer <an_gebrich@outlook.com>
2025-05-30 12:49:02 +0400

    add stub

---
+++ src/Stubs/DumpedActiveQueryStub.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Silencenjoyer\DumpActiveQuery\Stubs;
+
+use yii\db\ActiveQuery;
+
+/**
+ * IDE stub to provide autocompletion for dump methods via behavior.
+ *
+ * @method never ddCommand()
+ * @method never ddRawSql()
+ * @method never ddWhere()
+ * @method never ddWith()
+ * @method never ddJoinWith()
+ * @method never ddGroupBy()
+ * @method never ddHaving()
+ * @method never ddProps(array $props)
+ * @method never dd()
+ */
+class DumpedActiveQueryStub extends ActiveQuery
+{
+    // Just for IDE
+}
