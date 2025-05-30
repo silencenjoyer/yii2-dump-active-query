ef5530658b2fc5f120fb27ccdce69e47021e9ace
Sikencenjoyer <an_gebrich@outlook.com>
2025-05-30 12:25:10 +0400

    replace descendant to behavior

--- src/DumpActiveQueryModule.php
+++ src/DumpActiveQueryModule.php
@@ -2,6 +2,7 @@
 
 namespace Silencenjoyer\DumpActiveQuery;
 
+use Silencenjoyer\DumpActiveQuery\Components\Behaviors\DumpActiveQuery;
 use yii\base\Application;
 use yii\base\BootstrapInterface;
 use yii\base\Module;
@@ -10,16 +11,30 @@ use yii\db\ActiveQuery;
 class DumpActiveQueryModule extends Module implements BootstrapInterface
 {
     /**
+     * @var list<class-string>
+     */
+    public array $queryClasses = [];
+
+    /**
      * {@inheritDoc}
      * @param Application $app
      * @return void
      */
     public function bootstrap($app): void
     {
-        $app->setContainer([
-            'definitions' => [
-                ActiveQuery::class => Db\ActiveQuery::class,
-            ]
-        ]);
+        if (!in_array(ActiveQuery::class, $this->queryClasses)) {
+            $this->queryClasses[] = ActiveQuery::class;
+        }
+
+        foreach ($this->queryClasses as $queryClass) {
+            $app->setContainer([
+                'definitions' => [
+                    $queryClass => [
+                        'class' => $queryClass,
+                        'as dumpQuery' => DumpActiveQuery::class,
+                    ],
+                ]
+            ]);
+        }
     }
 }
